84c08081bf45ada14e52be5b295bf094a976dd47
SomeTimeCode <cryingcoding123@gmail.com>
2022-04-20 22:45:35 +0800

    nginx F5

--- backendNodeJS/src/services/supervisorServices.js
+++ backendNodeJS/src/services/supervisorServices.js
@@ -1,16 +1,28 @@
 const WebPA = (obj, student_list) => {
+    console.log(obj)
+    console.log(student_list)
     var student_scorce = {}
     student_list.forEach((student) => {
         student_scorce[student] = 0
     })
     // sum of student given
     Object.keys(obj).forEach((student) => {
-        var sum = Object.values(obj[student]).reduce((a, b) => parseInt(a) + parseInt(b))
+        var sum = Object.values(obj[student]).reduce((a, b) => {
+            if(b == null){
+                b = 0
+            }
+            console.log(b)
+            return parseInt(a) + parseInt(b)
+        })
         if(sum == 0){
             sum = 1
         }
         Object.keys(obj[student]).forEach((student_response)=>{
-            student_scorce[student_response] += parseInt(obj[student][student_response])/sum
+            if(obj[student][student_response] == null){
+                student_scorce[student_response] += 0
+            }else{
+                student_scorce[student_response] += parseInt(obj[student][student_response])/sum
+            }
         })
     })
     //update fraction
